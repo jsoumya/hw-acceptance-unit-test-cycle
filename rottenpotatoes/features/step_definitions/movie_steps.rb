f97c13d5537e7373d54b5c6b552574a09208600a
Soumya <soumyajyoti.jh@tamu.edu>
2018-03-26 21:57:24 +0000

    Final run

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,9 +1,5 @@
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
-    # hint: look at movies_controller#create for a hint toward the one-line solution.
-
 	  Movie.create!(movie)
   end
 end
